db4e8736bd83ba1c705c5bb30d0860bfba7d9e70
Tanner Morrison <tmorrison2552@gmail.com>
2022-07-28 00:35:54 -0600

    Initial Commit

---
+++ src/spacecraft.py
@@ -0,0 +1,8 @@
+class spacecraft:
+    def __init__(self,position,velocity,mass,name):
+        self.position = position
+        self.velocity = velocity
+        self.mass = mass
+        self.name = name
+
+        
